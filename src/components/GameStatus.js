0ea87bf08673ce028a51cdae759f3530d057dfdc
Alina Zolotavina <abekmetova@yandex.ru>
2023-11-07 00:36:19 +0400

    feat: build folder for gh-pages

--- src/components/GameStatus.js
+++
@@ -1,7 +0,0 @@
-function GameStatus({ status, winner }) {
-    return(
-        <p className={`status ${winner !== null ? 'status_winner' : ''}`}>{status}</p>
-    )
-};
-
-export default GameStatus;
